f5ab022b5e1bb648b643549b062be337a5ba35b5
MO <xmike.mox@gmail.com>
2025-02-15 13:55:12 -0300

    Paciente.Html

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.pacientes, name="pacientes")
+    path('', views.pacientes, name="pacientes"),
+    path('<int:id>', views.paciente_view, name="paciente_view")
 ]
