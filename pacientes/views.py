721c46eb8ac19640fdcd6185c0630cceb3dd2146
MO <xmike.mox@gmail.com>
2025-02-15 15:24:12 -0300

    Tuned queries

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Pacientes
+from .models import Pacientes, Tarefas, Consultas
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -34,7 +34,31 @@ def pacientes(request):
 def paciente_view(request, id):
     paciente = Pacientes.objects.get(id=id)
     if request.method == "GET":
-        return render(request, 'paciente.html', {'paciente': paciente})
+        tarefas = Tarefas.objects.all()
+        consultas = Consultas.objects.filter(paciente=paciente)
+        return render(request, 'paciente.html', {'paciente': paciente, 'tarefas': tarefas, 'consultas': consultas})
+    else:
+        humor = request.POST.get('humor')
+        registro_geral = request.POST.get('registro_geral')
+        video = request.FILES.get('video')
+        tarefas = request.POST.getlist('tarefas')
+
+        consultas = Consultas(
+            humor=int(humor),
+            registro_geral=registro_geral,
+            video=video,
+            paciente=paciente
+        )
+        consultas.save()
+
+        for i in tarefas:
+            tarefa = Tarefas.objects.get(id=i)
+            consultas.tarefas.add(tarefa)
+
+        consultas.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Registro de consulta adicionado com sucesso.')
+        return redirect(f'/pacientes/{id}')
 
 def atualizar_paciente(request, id):
     pagamento_em_dia = request.POST.get('pagamento_em_dia')
@@ -44,3 +68,8 @@ def atualizar_paciente(request, id):
     paciente.save()
 
     return redirect(f'/pacientes/{id}')
+
+def excluir_consulta(request, id):
+    consulta = Consultas.objects.get(id=id)
+    consulta.delete()
+    return redirect(f'/pacientes/{consulta.paciente.id}')
